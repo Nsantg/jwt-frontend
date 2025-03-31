1b5339e17598359189119e688e134ae9165bac93
Nsantg <nscarmona_170@cue.edu.co>
2025-03-31 16:11:14 -0500

    Updates

---
+++ precache-manifest.325a9cb3e58eff573a05d15a5cb0cc0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c691547b0c2fd87225d91d0c6dadc24b",
+    "url": "/jwt-frontend/index.html"
+  },
+  {
+    "revision": "973806eee73219583ef1",
+    "url": "/jwt-frontend/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "1827a7bc29eba4f1c46b",
+    "url": "/jwt-frontend/static/css/main.bfdb6209.chunk.css"
+  },
+  {
+    "revision": "973806eee73219583ef1",
+    "url": "/jwt-frontend/static/js/2.dbebe5bc.chunk.js"
+  },
+  {
+    "revision": "4fddd6a71b4f06691e2f08279cdb47f2",
+    "url": "/jwt-frontend/static/js/2.dbebe5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20931deaef9f0dcea2f5",
+    "url": "/jwt-frontend/static/js/3.12a56520.chunk.js"
+  },
+  {
+    "revision": "1827a7bc29eba4f1c46b",
+    "url": "/jwt-frontend/static/js/main.33047dab.chunk.js"
+  },
+  {
+    "revision": "7648dc63cb9eee4b2702",
+    "url": "/jwt-frontend/static/js/runtime-main.8a6a063b.js"
+  }
+]);
